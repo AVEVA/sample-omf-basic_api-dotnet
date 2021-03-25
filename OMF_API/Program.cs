ff9d3df4dff4d197ca842899c67f5e340c66c02f
Doug Broad <dbroad@osisoft.com>
2021-03-25 11:53:30 -0400

    Remove logging

--- OMF_API/Program.cs
+++ OMF_API/Program.cs
@@ -84,8 +84,6 @@ namespace OMF_API
             try
             {
                 //bring in configuration.  Note storing credentials in plain text is not secure or advised
-                var txt = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "/appsettings.json");
-                Console.Write(txt);
                 IConfigurationBuilder builder = new ConfigurationBuilder()
                  .SetBasePath(Directory.GetCurrentDirectory())
                  .AddJsonFile("appsettings.json")
